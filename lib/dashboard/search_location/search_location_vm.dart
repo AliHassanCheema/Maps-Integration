5c8c31184330e36549eeaf3beaf8494b2c014bc6
Ali Hassan Cheema <ahcheema12@gmail.com>
2023-10-27 16:41:31 +0500

    search location is under process

--- lib/dashboard/search_location/search_location_vm.dart
+++ lib/dashboard/search_location/search_location_vm.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:maps_integration/secrets.dart';
 import 'package:maps_integration/utils.dart';
 import 'package:stacked/stacked.dart';
@@ -8,10 +11,16 @@ import 'package:http/http.dart' as http;
 
 class SearchLocationVM extends BaseViewModel {
   final TextEditingController searchController = TextEditingController();
-  List<dynamic> searchResults = [];
+  List<LatLng> coordinates = [];
+  Set<Polyline> polylines = {};
+  Set<Marker> markers = {};
+  String? duration;
+  String? distance;
+  GoogleMapController? googleMapController;
+  List<SearchLocation> searchLocations = [];
   Future<void> onSearch(String query, context) async {
     if (query.isEmpty) {
-      searchResults.clear();
+      searchLocations.clear();
       notifyListeners();
       return;
     }
@@ -20,15 +29,84 @@ class SearchLocationVM extends BaseViewModel {
         Uri.parse(
             'https://maps.googleapis.com/maps/api/place/textsearch/json?key=$googleMapsApiKey&query=$query'),
         headers: {'Accept': 'application/json'});
-
     if (response.statusCode == 200) {
       final resp = json.decode(response.body);
       if (resp['status'] == 'OK') {
-        searchResults = resp['results'];
+        searchLocations.clear();
+        resp['results'].map((e) {
+          searchLocations.add(SearchLocation(
+              e['name'],
+              e['formatted_address'],
+              e['geometry']['location']['lat'],
+              e['geometry']['location']['lng']));
+        }).toList();
+        debugPrint('==========searchLocations========== $searchLocations');
         notifyListeners();
       } else {
         Utils.showSnackBar(context, resp['error_message']);
       }
     }
   }
+
+  onClickSearchedItem(SearchLocation searchLocation, BuildContext context) {
+    searchController.text = searchLocation.name;
+    FocusManager.instance.primaryFocus?.unfocus();
+    searchLocations.clear();
+    coordinates.add(LatLng(searchLocation.lat, searchLocation.lng));
+    onMapCreated(googleMapController!, context);
+  }
+
+  onMapCreated(GoogleMapController controller, context) async {
+    googleMapController ??= controller;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: coordinates.last,
+          zoom: 16.0,
+        ),
+      ),
+    );
+    markers
+        .add(Marker(markerId: const MarkerId('1'), position: coordinates.last));
+    notifyListeners();
+  }
+
+  onGetCurrentLocation(BuildContext context) async {
+    setBusy(true);
+    Position position = await Utils.getCurrentPosition(context);
+    coordinates.add(LatLng(position.latitude, position.longitude));
+    setBusy(false);
+  }
+
+  onMakePolyline() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        googleMapsApiKey,
+        PointLatLng(coordinates.first.latitude, coordinates.first.longitude),
+        PointLatLng(coordinates.last.latitude, coordinates.last.longitude));
+    if (result.points.isNotEmpty) {
+      distance = result.distance;
+      duration = result.duration;
+      coordinates =
+          result.points.map((e) => LatLng(e.latitude, e.longitude)).toList();
+      polylines.add(Polyline(
+        startCap: Cap.roundCap,
+        endCap: Cap.roundCap,
+        polylineId: const PolylineId('polyline_id'),
+        color: Colors.blue,
+        points: coordinates,
+        width: 5,
+      ));
+      notifyListeners();
+    }
+  }
+}
+
+class SearchLocation {
+  String name;
+  String address;
+  double lat;
+  double lng;
+
+  SearchLocation(this.name, this.address, this.lat, this.lng);
 }
--- lib/dashboard/search_location/search_location_vu.dart
+++ lib/dashboard/search_location/search_location_vu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import 'package:maps_integration/dashboard/search_location/search_location_vm.dart';
 import 'package:stacked/stacked.dart';
 
@@ -11,41 +12,139 @@ class SearchLocationVU extends StackedView<SearchLocationVM> {
   Widget builder(
       BuildContext context, SearchLocationVM viewModel, Widget? child) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search Location'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: viewModel.searchController,
-              decoration:
-                  const InputDecoration(labelText: 'Search for a place'),
-              onChanged: (value) {
-                viewModel.onSearch(value, context);
-              },
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: viewModel.searchResults.length,
-              itemBuilder: (context, index) {
-                final result = viewModel.searchResults[index];
-                return ListTile(
-                  title: Text(result['name']),
-                  subtitle: Text(result['formatted_address']),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Search Location'),
+        ),
+        body: viewModel.isBusy
+            ? const LinearProgressIndicator()
+            : Stack(
+                children: [
+                  _googleMapWidget(viewModel, context),
+                  Column(
+                    children: [
+                      _searchField(viewModel, context),
+                      viewModel.coordinates.length < 2
+                          ? const SizedBox.shrink()
+                          : ElevatedButton(
+                              onPressed: () {
+                                viewModel.onMakePolyline();
+                              },
+                              child: const Text('Find Directions')),
+                      viewModel.searchLocations.isEmpty
+                          ? const SizedBox.shrink()
+                          : searchList(viewModel),
+                    ],
+                  ),
+                ],
+              ),
+        bottomSheet: viewModel.distance != null && viewModel.duration != null
+            ? _pollyLineBottomSheet(viewModel)
+            : const SizedBox.shrink());
   }
 
   @override
   SearchLocationVM viewModelBuilder(BuildContext context) {
-    return SearchLocationVM();
+    final vm = SearchLocationVM();
+    vm.onGetCurrentLocation(context);
+    return vm;
+  }
+
+  Widget _pollyLineBottomSheet(SearchLocationVM viewModel) {
+    return Container(
+      decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 127, 99, 175),
+          borderRadius: BorderRadius.circular(12)),
+      height: 100,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _bottomSheetRow('Total Distance : ', viewModel.distance ?? '--'),
+            _bottomSheetRow('Time : ', viewModel.duration ?? '--'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchList(SearchLocationVM viewModel) {
+    return Expanded(
+      child: Container(
+        color: Colors.white,
+        child: ListView.builder(
+          itemCount: viewModel.searchLocations.length,
+          itemBuilder: (context, index) {
+            final result = viewModel.searchLocations[index];
+            return ListTile(
+              onTap: () {
+                viewModel.onClickSearchedItem(result, context);
+              },
+              title: Text(result.name),
+              subtitle: Text(result.address),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _searchField(SearchLocationVM viewModel, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SizedBox(
+        height: 52,
+        child: TextFormField(
+          controller: viewModel.searchController,
+          textAlignVertical: TextAlignVertical.bottom,
+          decoration: InputDecoration(
+              suffixIcon: const Icon(Icons.search),
+              fillColor: Colors.white,
+              filled: true,
+              enabledBorder:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(100)),
+              focusedBorder:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(100)),
+              hintText: 'Search for a place'),
+          onChanged: (value) {
+            viewModel.onSearch(value, context);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _googleMapWidget(SearchLocationVM viewModel, BuildContext context) {
+    return GoogleMap(
+      zoomControlsEnabled: false,
+      myLocationButtonEnabled: false,
+      myLocationEnabled: true,
+      mapType: mapType,
+      onMapCreated: (controller) {
+        viewModel.onMapCreated(controller, context);
+      },
+      markers: viewModel.markers,
+      polylines: viewModel.polylines,
+      initialCameraPosition: CameraPosition(
+        target: viewModel.coordinates.last,
+      ),
+    );
+  }
+
+  Widget _bottomSheetRow(String heading, String value) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          heading,
+          style: const TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        Text(
+          value,
+          style: const TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ],
+    );
   }
 }
